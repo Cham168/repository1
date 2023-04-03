6a8ad99834f53a3aa91d0291685277b1ce953413
Cham168 <skladchikov.nikitos.agios@gmail.com>
2023-04-03 20:14:12 +0300

    1
    done

---
+++ lesson3.py
@@ -0,0 +1,4 @@
+def my_abs(var):
+    if var < 0:
+        return var*(-1)
+    return var
---
+++ test.py
@@ -0,0 +1,23 @@
+def find_max (first1, second2):
+    find_max = first1 - second2
+    if find_max > second2:
+        return (first1)
+    else:
+        return (second2)
+
+
+"""
+Проверка:
+first1 = int(input('Enter "first"'))
+second2 = int(input('Enter "second"'))
+print (find_max(first1, second2))
+"""
+"""
+def min (first1, second2, third3):
+    min = first1 - second2
+    if min > 0:
+        return (first1)
+    elif:
+        return (second2)
+    else
+"""
